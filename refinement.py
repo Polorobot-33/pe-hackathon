449a3c745a253f19122de3d4d13132fc9c8769b2
Paul Fraenkel <paul.fraenkel@etu.minesparis.psl.eu>
2024-10-29 15:12:02 +0100

    refinement amelioration

--- refinement.py
+++ refinement.py
@@ -4,6 +4,7 @@ import pandas as pd
 def load_table() :
     df = pd.read_csv('PS_2024.10.29_06.12.44.csv', sep=',', header=96)
     df.drop_duplicates(inplace=True)
-    return df[df.default_flag == 1].drop(columns='default_flag')
-
-#print(load_table().describe())
+    df = df[df.default_flag == 1].drop(columns='default_flag')
+    df = df[['pl_name', 'disc_year', 'discoverymethod', 'pl_orbper', 'pl_orbsmax', 'pl_rade', 'pl_bmasse', 'pl_orbeccen', 'pl_insol', 'pl_eqt', 'st_mass', 'st_teff']]
+    df.set_index('pl_name', inplace=True)
+    return df
