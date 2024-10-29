7c25d39f5f311cd03feb386a0835e612067ee6e3
Guillaume Bardinet <guillaume.bardinet@etu.minesparis.psl.eu>
2024-10-29 16:55:12 +0100

    Nouveau fichier Kepler

---
+++ 3eme_loi_Kepler.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import refinement as rf
+df=rf.load_table()
+#df=df[df['pl_name']!='TOI-1694 c']
+un_sur_M=1/df['st_mass']
+T2=df['pl_orbper']**2
+a3=df['pl_orbsmax']**3
+F=T2/a3
+plt.plot(un_sur_M,F,marker='+',linewidth=0)
+plt.xlabel('1/M_soleil')
+plt.ylabel('T^2/a^3')
+plt.title('Validation expérimentale de la 3ème loi de Kepler')
